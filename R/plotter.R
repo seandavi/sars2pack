a5d3031516f0f7998ebc0be9e0dc0f389b0bcc60
Vince Carey <stvjc@channing.harvard.edu>
2020-03-19 17:26:34 -0400

    moving on

--- R/plotter.R
+++ R/plotter.R
@@ -40,3 +40,70 @@ plot2.R0.sR = function (x, xscale = "w", TD.split = FALSE, ...)
     }
 }
 
+#' generic for simpler plotter for plotfit for R0.sR
+#' @param x R0.sR instance
+#' @param all logical(1)
+#' @param xscale character code for x axis scale
+#' @param SB.dist logical(1)
+#' @param \dots passed to base::plot
+#' @export
+plotfit2 = function(x, all=TRUE, xscale="w", SB.dist=TRUE, ...) UseMethod("plotfit2")
+
+#' version of R0:::plotfit.R0.sR that does not use dev.new
+#' @param x instance of R0.sR
+#' @param all logical(1)
+#' @param xscale character(1) scale to be used on x axis (d = daily, w=weekly, f=fortnightly, m=monthly)
+#' @param SB.dist logical(1)
+#' @param \dots passed to base::plot
+#' @export
+plotfit2.R0.sR = function (x, all = TRUE, xscale = "w", SB.dist = TRUE, ...) 
+{
+    if (class(x) != "R0.sR") {
+        stop("'x' must be of class 'sR'")
+    }
+    if (xscale != "d" & xscale != "w" & xscale != "f" & xscale != 
+        "m") {
+        stop("Invalid scale parameter.")
+    }
+    if (exists("EG", where = x$estimates)) {
+        plotfit(x$estimates$EG, xscale = xscale, ...)
+    }
+    if (exists("ML", where = x$estimates)) {
+        plotfit(x$estimates$ML, xscale = xscale, ...)
+    }
+    if (exists("TD", where = x$estimates)) {
+        plotfit(x$estimates$TD, xscale = xscale, ...)
+    }
+    if (exists("SB", where = x$estimates)) {
+        plotfit(x$estimates$SB, xscale = xscale, SB.dist = SB.dist, 
+            ...)
+    }
+}
+
+#' supersimple series extraction
+#' @importFrom magrittr "%>%"
+#' @importFrom dplyr filter
+#' @param province character(1) must be found in dataset ProvinceState field, "" is typical for data aggregated only at country level, and that is the default
+#' @param country character(1) must be found in CountryRegion field
+#' @param dataset data.frame as returned by fetch_JHU_Data
+#' @export
+get_series = function(province="", country, dataset=sars2pack::mar19df) {
+  stopifnot(all(c("ProvinceState", "CountryRegion") %in% colnames(dataset)))
+  stopifnot(country %in% dataset$CountryRegion)
+  ans = dataset %>% dplyr::filter(ProvinceState==province & CountryRegion==country)
+  ans[,-c(1:4)]
+}
+
+#' supersimple series plotter
+#' @importFrom lubridate as_date mdy
+#' @param province character(1) must be found in dataset ProvinceState field, "" is typical for data aggregated only at country level, and that is the default
+#' @param country character(1) must be found in CountryRegion field
+#' @param dataset data.frame as returned by fetch_JHU_Data
+#' @param \dots passed to base::plot
+#' @export
+plot_series = function(province="", country, dataset=sars2pack::mar19df, ...) {
+ ser = get_series(province=province, country=country, dataset=dataset)
+ dates = lubridate::as_date(mdy(names(dataset)[-c(1:4)]))
+ plot(dates, ser, main=paste(province, country), ...)
+}
+
