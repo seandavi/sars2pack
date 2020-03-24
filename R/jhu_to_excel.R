bad3a06c23a76445a0dca7b869088573aed82510
Sean Davis <seandavi@gmail.com>
2020-03-23 22:06:57 -0400

    add documentation, country metadata functionality

--- R/jhu_to_excel.R
+++ R/jhu_to_excel.R
@@ -1,12 +1,65 @@
+#' utility function to remove columns that contain `data.frame` type
+#'
+#' The dplyr::bind_rows function does not work with
+#' `data.frame` columns, so simply remove them as needed.
+#'
+.remove_df_columns <- function(df) {
+    df_cols = which(sapply(df, class)=='data.frame')
+    df[, -c(df_cols)]
+}
+
+.cols_to_remove = c(
+    "callingCodes",
+    "altSpellings",
+    "latlng",
+    "demonym",
+    "timezones",
+    "nativeName",
+    "currencies",
+    "languages",
+    "translations",
+    "flag",
+    "regionalBlocs"
+)
+
+#' Get enriched jhu dataset as a list of three data.frames
+#'
+#' The purpose of this function is to enrich the JHU dataset
+#' with additional country-level metadata and then get the
+#' data into a form that can be easily written to excel or used
+#' as a "wide-format" tabular dataset.
 #'
 #' @importFrom openxlsx write.xlsx
+#' @importFrom countrycode countrycode
 #'
+#' @param cols_to_remove a character vector of column names from
+#' [country_metadata()] to remove.
+#' 
+#' @return A list of three `data.frames` named `deaths`, `confirmed`,
+#' and `recovered`.
+#' 
 #'
-jhu_data_to_excel <- function(include_country_metadata=TRUE) {
+enriched_jhu_data <- function(cols_to_remove = .cols_to_remove) {
     res = jhu_data()
-    ret = lapply(unique(res$subset),function(x) {
-        res2 = res %>% dplyr::filter(subset==x)
-        res3 = res2 %>% tidyr::pivot_wider(names_from = date, values_from = count)
+    cmd = country_metadata()
+    subsets = unique(res$subset)
+    ret = lapply(subsets,function(x) {
+        res2 = res %>%
+            dplyr::filter(subset==x) %>%
+            tidyr::pivot_wider(names_from = date, values_from = count) %>%
+            dplyr::select(-subset)
+        res2$alpha3Code = countrycode(
+            res2$CountryRegion,
+            origin = "country.name.en",
+            destination="iso3c",
+            ## This is a custom HACK
+            custom_match=c('Kosovo'='XKX', 'Cruise Ship'=NA))
+        res3 = cmd %>%
+            dplyr::right_join(res2,by=c("alpha3Code"="alpha3Code")) %>%
+            dplyr::select(-c(cols_to_remove))
+        if(nrow(res3) != nrow(res2)) {
+            stop('row numbers do not match')
+        }
         return(res3)
     })
     names(ret) = unique(res$subset)
