04a674289dcb84dd868e26e2758216b20bd46486
Sean Davis <seandavi@gmail.com>
2020-04-17 00:45:01 -0400

    add state distancing policy
    closes #28
    
    Thanks, @mymil!

---
+++ R/us_state_distancing_policy.R
@@ -0,0 +1,30 @@
+#' United States social distancing policies for COVID-19
+#' 
+#' An updated, curated set of social distancing and non-pharmaceutical interventions
+#' for a subset of the states in the United States.
+#' 
+#' @importFrom readr read_csv
+#' 
+#' @source 
+#' - https://github.com/COVID19StatePolicy/SocialDistancing/
+#' 
+#' @section Kudos:
+#' - Joe Wasserman: http://www.joewasserman.com/
+#' 
+#' 
+#' @author Sean Davis <seandavi@gmail.com>
+#' 
+#' @family data-import
+#' 
+#' @examples 
+#' res = us_state_distancing_policy()
+#' colnames(res)
+#' library(dplyr)
+#' dplyr::glimpse(res)
+#' 
+#' @export
+us_state_distancing_policy = function() {
+    rpath = s2p_cached_url('https://raw.githubusercontent.com/COVID19StatePolicy/SocialDistancing/master/data/USstatesCov19distancingpolicy.csv')
+    res = readr::read_csv(rpath,col_types = cols())
+    res
+}
