01c26776e7bd007eff067e60062ac807153038b6
Sean Davis <seandavi@gmail.com>
2020-04-18 17:26:11 -0400

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,2 +1,6 @@
 install.packages(c("BiocManager", "remotes"))
+# this is to shortcut the errors during docker
+# testing. sf will be installed as a dependency
+# without the next line.
+install.packages('sf')
 BiocManager::install(c("seandavi/sars2pack","plumber","EpiEstim", "urltools","jsonlite"))
