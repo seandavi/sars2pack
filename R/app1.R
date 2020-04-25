a75125f08d1ee8e0f1e0c880e70b21a1cb5f3cd3
Vince Carey <stvjc@channing.harvard.edu>
2020-04-25 09:56:14 -0400

    doMCMC.Rmd is done

--- R/app1.R
+++ R/app1.R
@@ -2,18 +2,31 @@
 #' @import shiny
 #' @import EpiEstim
 #' @param src intended to be the result of sars2pack::nytimes_state_data(), no checking is done.
-#' @note This app is currently incomplete.  Additions needed include 1) accommodation of
-#' standard deviation of parametric serial interval distribution, 2) additional
+#' @param mean_si_trunc_radius numeric(1) 
+#' @param std_si_trunc_radius numeric(1) 
+#' @param std_mean_si numeric(1) used with 'uncertain' serial interval option, see EpiEstim::make_config
+#' @param std_std_si numeric(1) used with 'uncertain' serial interval option, see EpiEstim::make_config
+#' @param n1 numeric(1) used with 'uncertain' serial interval option, see EpiEstim::make_config
+#' @param n2 numeric(1) used with 'uncertain' serial interval option, see EpiEstim::make_config
+#' @note This app is currently incomplete.  Additions needed include 
+#' 2) additional
 #' approaches to serial interval modeling, 3) enhancement of displays to present actual
 #' calendar dates, 4) an approach to supporting comparative displays, particularly allowing
 #' overlay of different R[t] fits for different serial interval modeling assumptions.
 #' @examples
-#' \dontrun{
+#' # must run example(eesi, ask=FALSE)
+#' if (interactive()) {
 #'  nytdat = nytimes_state_data()
 #'  eesi(nytdat)
 #' }
 #' @export
-eesi = function(src) {  # intended to run with nyt state data
+eesi = function(src,
+ mean_si_trunc_radius = 3,
+ std_si_trunc_radius = 3,
+ std_mean_si = 3, 
+ std_std_si = 5,
+ n1 = 100,
+ n2 = 100) {  # intended to run with nyt state data
  states = sort(unique(src$state))
  ui = fluidPage(
   sidebarLayout(
@@ -29,11 +42,13 @@ eesi = function(src) {  # intended to run with nyt state data
     dateInput("inidat", "Start of outbreak", value="2020-03-15", min="2020-01-01",
        max="2020-04-01"),
     radioButtons("sitype", "Serial Interval Method",
-      choices=c("2009 flu", "parametric"), selected="2009 flu"),
+      choices=c("2009 flu", "uncertain"), selected="2009 flu"),
     conditionalPanel(
-     condition = "input.sitype == 'parametric'",
+     condition = "input.sitype == 'uncertain'",
        numericInput("parmn", "mean SI", min=1, max=6, step=.1,
-                      value=3.8)
+                      value=4),
+       numericInput("parmnsd", "SD SI", min=.5, max=12, step=.1,
+                      value=5)
      ),
     numericInput("winsz", "window size", min=1, max=14, step=1,
                    value=7),
@@ -67,8 +82,13 @@ eesi = function(src) {  # intended to run with nyt state data
                         config = make_config(list(si_distr = Flu2009$si_distr,
                           t_start=starts, t_end=ends)))
      } else {
-     ans = EpiEstim::estimate_R(incid = icur$count, method="parametric_si",
-      config=make_config(list(mean_si=input$parmn, std_si=4.8,
+     print(c(minm=input$parmn-mean_si_trunc_radius, m=input$parmn, maxm=input$parmn+mean_si_trunc_radius))
+     ans = EpiEstim::estimate_R(incid = icur$count, method="uncertain_si",
+      config=make_config(list(mean_si=input$parmn, std_si=input$parmnsd,
+             std_mean_si=std_mean_si, min_mean_si=max(c(1,input$parmn-mean_si_trunc_radius)),
+             max_mean_si=input$parmn+mean_si_trunc_radius,
+             std_std_si=std_std_si, min_std_si=max(c(.5, input$parmnsd-std_si_trunc_radius)),
+            max_std_si=input$parmnsd+std_si_trunc_radius, n1=n1, n2=n2,
                           t_start=starts, t_end=ends))) 
      }
     ans
--- R/data.R
+++ R/data.R
@@ -11,3 +11,37 @@
 #' @examples
 #' mar19dt[1:3, 1:8]
 "mar19dt"
+
+#' serial interval values fo 468 COVID-19 cases in 93 cities in China
+#' @source \url{https://github.com/MeyersLabUTexas/COVID-19} Table S5.xlsx
+#' @format data.frame
+"CRF_468_China"
+
+
+#' MCMC-based model for SI, Gamma family used for censored-data likelihood
+#' @format list with components `si_sample` (matrix) and `si_parametric_distr (character)
+"si_cens_G"
+
+#' MCMC-based model for SI, lognormal family used for censored-data likelihood
+#' @format list with components `si_sample` (matrix) and `si_parametric_distr (character)
+"si_cens_L"
+
+#' MCMC-based model for SI, Weibull family used for censored-data likelihood
+#' @format list with components `si_sample` (matrix) and `si_parametric_distr (character)
+"si_cens_W"
+
+
+#' MCMC-based model for SI, Gamma family used for positive-data likelihood
+#' @note The data reported at [CDC](https://wwwnc.cdc.gov/eid/article/26/6/20-0357_article) on
+#' 468 transmission events were filtered to include only reports where contact
+#' symptom onset was later than that reported for index case.
+#' @format list with components `si_sample` (matrix) and `si_parametric_distr (character)
+"si_pos_G"
+
+#' MCMC-based model for SI, lognormal family used for positive-data likelihood
+#' @format list with components `si_sample` (matrix) and `si_parametric_distr (character)
+"si_pos_L"
+
+#' MCMC-based model for SI, Weibull family used for positive-data likelihood
+#' @format list with components `si_sample` (matrix) and `si_parametric_distr (character)
+"si_pos_W"
