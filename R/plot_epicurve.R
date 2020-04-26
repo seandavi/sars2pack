779f6f136b40a9c1bb5230b1d55c2c818ab8826e
Sean Davis <seandavi@gmail.com>
2020-04-26 14:05:42 -0400

    epicurve update

--- R/plot_epicurve.R
+++ R/plot_epicurve.R
@@ -6,7 +6,6 @@
 #' 
 #' - a date column (or any data type that has a natural time order); this will become the x-axis
 #' - a cumulative `count` column; this will become the y-axis
-#' - an optional grouping column to produce multiple colored curves; y-axis curves will be colored by this column
 #' 
 #' @importFrom dplyr %>% group_by_ summarize filter
 #' @importFrom ggplot2 ggplot aes_string geom_line scale_y_log10
@@ -15,37 +14,25 @@
 #' least a date column and an integer count column
 #' @param date_column character(1) the column name of the `date` type column
 #' @param case_column character(1) the column name of the `count of cases` column
-#' @param grouping_column character(1) the optional column name of the `grouping` column,
-#' typically for drawing multiple countries or states, for example
 #' @param log logical(1) TRUE for log10 based y-scale, FALSE for linear
+#' @param ... passed to ggplot2::aes
 #' 
 #' @examples 
 #' library(dplyr)
 #' jhu = jhu_data() %>% 
-#'     filter(CountryRegion %in% c('US','Italy',"China") & subset=='confirmed') %>% 
+#'     filter(CountryRegion=='China' & subset=='confirmed') %>% 
 #'     group_by(CountryRegion,date) %>% summarize(count=sum(count)) 
 #'     
 #' jhu %>% plot_epicurve(log=FALSE)
-#' jhu %>% plot_epicurve(grouping_column='CountryRegion')
-#' jhu %>% plot_epicurve(grouping_column='CountryRegion', log=TRUE)
-#'
+#' 
 #' 
 #' @export
 plot_epicurve <- function(df, date_column = 'date', case_column = 'count', 
-                          grouping_column = NA, 
                           ..., log=TRUE, shift = FALSE) {
-    if(!is.na(grouping_column)) {
-        df = df %>% 
-            dplyr::group_by_(grouping_column, date_column) %>% 
-            dplyr::mutate(count=sum(.data[[case_column]]))
-    }
     if(log) {
         df = df %>% dplyr::filter(count>0)
     }
-    p = ggplot(df, aes_string(x=date_column, y=case_column))
-    if(!is.na(grouping_column)) {
-        p = ggplot(df, aes_string(x=date_column, y=case_column, color=grouping_column))
-    }
+    p = ggplot(df, aes_string(x=date_column, y=case_column, ...))
     p = p + geom_line()
     if(log) {
         p = p + scale_y_log10()
