7469a0253999be6493b81b1f6680e00c14be9d82
Sean Davis <seandavi@gmail.com>
2020-05-01 12:38:08 -0400

    fix phantomjs bug

--- R/apple_mobility_data.R
+++ R/apple_mobility_data.R
@@ -41,7 +41,6 @@
 #'
 #' @importFrom RSelenium rsDriver
 #' @importFrom readr read_csv
-#' @importFrom wdman phantomjs
 #' @importFrom dplyr `%>%` mutate
 #' @importFrom tidyr pivot_longer
 #' 
@@ -102,7 +101,6 @@ apple_mobility_data = function(agree_to_terms=TRUE, max_tries=3,
     ## The code below uses webdriver to render the
     ##
     stopifnot(agree_to_terms)
-    pjs = wdman::phantomjs(port = 4444L)
     Sys.sleep(2)
     remDr <- RSelenium::remoteDriver(browserName = 'phantomjs')
     remDr$open(silent = TRUE)
