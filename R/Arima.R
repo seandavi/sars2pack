a1f29a78044b5dcf17b42d92dd5a0566616f1a20
Vince Carey <stvjc@channing.harvard.edu>
2020-05-26 08:14:45 -0400

    updates 0.1.0 with revised Arima, tsapp, BIC computations, est_Rt

--- R/Arima.R
+++ R/Arima.R
@@ -1,4 +1,83 @@
+
+#' a two-dimensional grid of integers
+#' @param n maximum value, will expand.grid(0:n,0:n) with colnames arorder, maorder
+#' @export
+grid_2d = function(n=5) expand.grid(arorder=seq(0,n), maorder=seq(0,n))
+
+#' minimize BIC for ARIMA models with default differencing order 1 over choices of MA and AR order
+#' @param src tibble with cumulative incidence like output of nytimes_state_data()
+#' @param fullusa logical(1) if TRUE, use Arima_nation (src should be enriched_jhu_data())
+#' @param state.in character(1) state name
+#' @param parms two-column data frame with proposed AR order in column 1 and MA order in column 2
+#' @param max_date character(1) or lubridate date from which to look back
+#' @param lookback_days numeric(1)
+#' @param \dots passed to `Arima_by_state`
+#' @examples
+#' nyd = nytimes_state_data()
+#' mb = min_bic(nyd, state.in="New York")
+#' names(mb)
+#' @export
+min_bic = function(src, fullusa=FALSE, state.in="New York", parms=grid_2d(5), max_date=NULL, 
+   lookback_days=29, ...) {
+ nr = nrow(parms)
+ if (!fullusa) bics = sapply(seq_len(nr), function(r) try(Arima_by_state(src, state.in=state.in, ARorder=parms$arorder[r],
+          MAorder=parms$maorder[r], max_date=max_date, lookback_days=lookback_days, ...)$fit$bic))
+ else {
+       state.in = NULL
+       bics = sapply(seq_len(nr), function(r) try(Arima_nation(src, ARorder=parms$arorder[r],
+          MAorder=parms$maorder[r], max_date=max_date, lookback_days=lookback_days, ...)$fit$bic))
+      }
+ errs = sapply(bics, inherits, "try-error")
+ dr = which(errs)
+ if (length(dr)>0) bics=bics[-dr]
+ ind = which.min(bics)
+ md = max_date
+ if (is.null(md)) md = max(src$date)
+ ans = list(opt=c(ARord=parms$arorder[ind], MAord=parms$maorder[ind]), bics=bics, fullusa=fullusa,
+                 state=state.in, date_run=Sys.Date(), max_date=md, lookback_days=lookback_days)
+ class(ans) = "bic_seq"
+ ans
+}
+
+#' report for optimal AR/MA search using BIC
+#' @param x instance of bic_seq S3 class
+#' @param \dots not used
+#' @export
+print.bic_seq = function(x, ...) {
+ cat(paste("bic_seq for", x$state, "; last date used =", lubridate::as_date(x$max_date), "\n"))
+ cat(" ", x$lookback_days, "day lookback.\n")
+ cat(" best BIC =", min(x$bics), "for AR order", x$opt["ARord"], "MA order", x$opt["MAord"], "\n")
+}
+
+#' organize search over contiguous states for optimal statewise AR/MA using BIC 
+#' @param src tibble with cumulative incidence like output of nytimes_state_data()
+#' @param \dots passed to `min_bic`
+#' @return instance of S3 class `min_bic_all_states`
+#' @export
+min_bic_all_states = function(src, ...) {
+ ans = lapply(contig_states_dc(), function(x) min_bic(src, state.in=x, ...))
+ names(ans) = contig_states_dc()
+ class(ans) = "min_bic_all_states"
+ ans
+}
+
+#' @export
+print.min_bic_all_states = function(x, ...) {
+ cat("min BICs for", x[[1]]$lookback_days, "day lookback from", 
+    paste(lubridate::as_date(x[[1]]$max_date), "\n"))
+ ars = sapply(x, function(x) x$opt["ARord"])
+ mas = sapply(x, function(x) x$opt["MAord"])
+ cat(" distribution of optimal autoregressive order:\n")
+ print(table(ars))
+ cat(" distribution of optimal moving average order:\n")
+ print(table(mas))
+}
+
 #' provide vector of contiguous US state names and DC
+#' @importFrom graphics abline
+#' @importFrom stats coef fitted.values
+#' @import dplyr
+#' @import magrittr
 #' @export
 contig_states_dc = function() c("Alabama", "Arizona", "Arkansas", "California", "Colorado", 
 "Connecticut", "Delaware", "District of Columbia", "Florida", 
@@ -52,18 +131,20 @@ make_cumul_events = function(count, dates,
     ans 
 }
 
-form_inc_state = function(src, regtag) {
+form_inc_state = function(src, regtag, max_date=NULL) {
  fullsumm = src %>% 
   dplyr::select(state,date,count) %>% group_by(date) %>% 
    summarise(count=sum(count))  # counts by date collapsed over states
+ if (!is.null(max_date)) fullsumm = filter(fullsumm, date <= lubridate::as_date(max_date))
  thecum = make_cumul_events(count=fullsumm$count, dates=fullsumm$date, regtag=regtag)
  form_incident_events(thecum)
 }
 
-form_inc_nation = function(src, regtag) {
+form_inc_nation = function(src, regtag, max_date=NULL) {
  fullsumm = src %>% 
   dplyr::select(date,count) %>% group_by(date) %>% 
    summarise(count=sum(count))  # counts by date 
+ if (!is.null(max_date)) fullsumm = filter(fullsumm, date <= lubridate::as_date(max_date))
  thecum = make_cumul_events(count=fullsumm$count, dates=fullsumm$date, regtag=regtag)
  form_incident_events(thecum)
 }
@@ -74,37 +155,44 @@ form_inc_nation = function(src, regtag) {
 #' @param src a tibble as returned by nytimes_state_data() or jhu_us_data()
 #' @param state.in character(1) state name
 #' @param MAorder numeric(1) order of moving average component
-#' @param trend character(1) if "linear" differencing order for ARIMA=1, else 2
+#' @param Difforder numeric(1) order of differencing d in ARIMA(p,d,q)
 #' @param basedate character(1) used by lubridate::as_date to filter away all earlier records
 #' @param lookback_days numeric(1) only uses this many days from most recent in src
 #' @param ARorder order of autoregressive component
+#' @param max_date a date from which to start lookback ... defaults to NULL in which
+#' case the latest available date is used
 #' @return instance of S3 class Arima_sars2pack
 #' @examples
 #' nyd = nytimes_state_data()
-#' lkny = Arima_by_state(nyd)
+#' mb = min_bic(nyd, state.in="New York")
+#' lkny = Arima_by_state(nyd, ARorder=mb$opt["ARord"], MAorder=mb$opt["MAord"])
 #' lkny
 #' plot(lkny)
 #' usd = jhu_us_data()
-#' lkny2 = Arima_by_state(usd)
+#' lkny2 = Arima_by_state(usd, ARorder=mb$opt["ARord"], MAorder=mb$opt["MAord"])
 #' lkny2
 #' plot(lkny2)
 #' @export
 Arima_by_state = function(src, state.in="New York", MAorder=2, 
-   trend="linear", basedate="2020-03-15", lookback_days=29, ARorder=0) {
+   Difforder=1, basedate="2020-02-15", lookback_days=29, ARorder=0, max_date=NULL) {
    cbyd = dplyr::filter(src, date >= basedate & subset=="confirmed" & state==state.in) 
-   ibyd = form_inc_state(cbyd, regtag=state.in)
+   ibyd = form_inc_state(cbyd, regtag=state.in, max_date=max_date)
+   tc = match.call()
    .Arima_inc(ibyd, state.in=state.in, MAorder=MAorder,
-      trend=trend, basedate=basedate, lookback_days=lookback_days, ARorder=ARorder)
+      Difforder=Difforder, basedate=basedate, lookback_days=lookback_days, ARorder=ARorder,
+      max_date=max_date, topcall=tc)
    }
 
 #' Use Rob Hyndman's forecast package to estimate drift in ARIMA models
 #' @param ejhu a tibble as returned by `enriched_jhu_data()`
 #' @param alp3 character(1) alpha3Code value for country
 #' @param MAorder numeric(1) order of moving average component
-#' @param trend character(1) if "linear" differencing order for ARIMA=1, else 2
+#' @param Difforder numeric(1) differencing order d of ARIMA(p,d,q)
 #' @param basedate character(1) used by lubridate::as_date to filter away all earlier records
 #' @param lookback_days numeric(1) only uses this many days from most recent in ejhu
 #' @param ARorder order of autoregressive component
+#' @param max_date a date from which to start lookback ... defaults to NULL in which
+#' case the latest available date is used
 #' @return instance of S3 class Arima_sars2pack
 #' @examples
 #' ej = enriched_jhu_data()
@@ -113,36 +201,40 @@ Arima_by_state = function(src, state.in="New York", MAorder=2,
 #' plot(lkus)
 #' @export
 Arima_nation = function(ejhu, alp3="USA", MAorder=2,
-   trend="linear", basedate="2020-03-15", lookback_days=29, ARorder=0) {
+   Difforder=1, basedate="2020-02-15", lookback_days=29, ARorder=0, max_date=NULL) {
    cbyd = dplyr::filter(ejhu, date >= basedate & subset=="confirmed" & alpha3Code==alp3)
-   ibyd = form_inc_nation(cbyd, regtag=alp3)
+   ibyd = form_inc_nation(cbyd, regtag=alp3, max_date=max_date)
+   tc = match.call()
    .Arima_inc(ibyd, state.in=alp3, MAorder=MAorder,
-      trend=trend, basedate=basedate, lookback_days=lookback_days, ARorder=ARorder)
+      Difforder=Difforder, basedate=basedate, lookback_days=lookback_days, ARorder=ARorder,
+        max_date=max_date, topcall=tc)
    }
 
 .Arima_inc = function(ibyd, state.in="New York", MAorder=2, 
-   trend="linear", basedate="2020-03-15", lookback_days=29, ARorder=0) {
+   Difforder=1, basedate="2020-02-15", lookback_days=29, ARorder=0, max_date=NULL, topcall=NULL) {
+   if (is.null(MAorder) | is.null(ARorder)) stop("MA/AR order inputs cannot be NULL")
    iuse = trim_from(ibyd, basedate)
    full29 = tail(ibyd$count,lookback_days)
    dates29 = tail(ibyd$date,lookback_days)
    nlb=lookback_days-1
    time = (0:nlb)/nlb
-#
-# interactive selection of quadratic or linear trend, MA order 
-#
-   difforder = ifelse(trend=="linear", 1, 2)
-   MAord2use = MAorder
-#
-# full basis matrix, which is filtered below to reduce trig order or trend type
 
    origin = max(ibyd$date)-lookback_days+1
    time.from.origin = as.numeric(dates29-origin)
    tsfull = ts(full29, freq=1)
-   Arima.full = Arima(tsfull, order=c(ARorder,difforder,MAord2use), include.drift=TRUE)
+   Arima.full = try(Arima(tsfull, order=c(ARorder,Difforder,MAorder), include.drift=TRUE))
+   if (inherits(Arima.full, "try-error")) {
+     print(c(ARorder,Difforder,MAorder))
+     print(Arima.full)
+     ans = "likely_AR_error"
+     class(ans) = c("AR_error", "try-error")
+     return(ans)
+     }
    pr = fitted.values(forecast(Arima.full))
    ans = list(fit=Arima.full, pred=pr, tsfull=tsfull, dates29=dates29, time.from.origin=time.from.origin,
-        call=match.call(),
-        state=state.in, origin=as_date(origin))
+        call=topcall,
+        state=state.in, origin=as_date(origin), MAorder=MAorder, Difforder=Difforder, ARorder=ARorder,
+            max_date=max_date, Arima.inc.call=match.call())
    class(ans) = "Arima_sars2pack"
    ans
 }
@@ -150,6 +242,7 @@ Arima_nation = function(ejhu, alp3="USA", MAorder=2,
 #' @export
 print.Arima_sars2pack = function(x, ...) {
  cat("Arima_sars2pack instance for", x$state, "\n  computed", date(),  "\n")
+ cat(paste("  last date used was: ", lubridate::as_date(max(x$dates29)), "\n"))
  cat("  call was: ")
  print(x$call)
  cat("  Model estimates: \n")
@@ -162,16 +255,18 @@ plot.Arima_sars2pack = function(x, y, ...) {
  x_ = x$origin+x$time.from.origin
  plot(x_, y_, pch=19, xlab="date", ylab="incidence", ...)
  lines(x$origin+x$time.from.origin, x$pred)
- slo = coef(x$fit)["drift"]
- y1 = x$pred[29]
- y0 = y1 + slo*(-as.numeric(x$origin)-28) # x$time.from.origin[29] = 28
- abline(y0, slo, lty=2)
- se = sqrt(x$fit$var.coef["drift", "drift"])
- y1b = slo*(14+as.numeric(x$origin))+y0
- y0p = y1b + (slo+1.96*se)*(-as.numeric(x$origin)-14) # x$time.from.origin[1] = 0
- y0m = y1b + (slo-1.96*se)*(-as.numeric(x$origin)-14) # x$time.from.origin[1] = 0
- abline(y0p, slo+1.96*se, lty=3)
- abline(y0m, slo-1.96*se, lty=3)
+ if (x$Difforder==1) {
+  slo = coef(x$fit)["drift"]
+  y1 = median(y_) #x$pred[29]
+  y0 = y1 + slo*(-as.numeric(x$origin)-13) # x$time.from.origin[29] = 28
+  abline(y0, slo, lty=2, lwd=2)
+  se = sqrt(x$fit$var.coef["drift", "drift"])
+  y1b = slo*(14+as.numeric(x$origin))+y0
+  y0p = y1b + (slo+1.96*se)*(-as.numeric(x$origin)-14) # x$time.from.origin[1] = 0
+  y0m = y1b + (slo-1.96*se)*(-as.numeric(x$origin)-14) # x$time.from.origin[1] = 0
+  abline(y0p, slo+1.96*se, lty=3, col="gray")
+  abline(y0m, slo-1.96*se, lty=3, col="gray")
+ }
 }
 
 #' fit ARIMA model to US data dropping one state
@@ -179,10 +274,12 @@ plot.Arima_sars2pack = function(x, y, ...) {
 #' @param src_st tibble for state level data like that of nytimes_state_data()
 #' @param state.in character(1) state name
 #' @param MAorder numeric(1) order of moving average component
-#' @param trend character(1) if "linear" differencing order for ARIMA=1, else 2
+#' @param Difforder numeric(1) differencing order d of ARIMA(p,d,q)
 #' @param basedate character(1) used by lubridate::as_date to filter away all earlier records
 #' @param lookback_days numeric(1) only uses this many days from most recent in src
 #' @param ARorder order of autoregressive component
+#' @param max_date a date from which to start lookback ... defaults to NULL in which
+#' case the latest available date is used
 #' @return instance of S3 class Arima_sars2pack
 #' @note Apparent discrepancies in counts in vicinity of April 15 between
 #' NYT and JHU for full US incidence lead us to employ the two sources
@@ -204,21 +301,45 @@ plot.Arima_sars2pack = function(x, y, ...) {
 #' par(opar)
 #' @export
 Arima_drop_state = function(src_us, src_st, state.in="New York", MAorder=2, 
-   trend="linear", basedate="2020-03-15", lookback_days=29, ARorder=0) {
-   nat = Arima_nation(src_us, MAorder=MAorder, trend=trend, basedate=basedate,
-         lookback_days=lookback_days, ARorder=ARorder)
-   st = Arima_by_state(src_st, state.in=state.in, MAorder=MAorder, trend=trend, basedate=basedate,
-         lookback_days=lookback_days, ARorder=ARorder)
+   Difforder=1, basedate="2020-02-15", lookback_days=29, ARorder=0, max_date=NULL) {
+   nat = Arima_nation(src_us, MAorder=MAorder, Difforder=Difforder, basedate=basedate,
+         lookback_days=lookback_days, ARorder=ARorder, max_date=max_date)
+   st = Arima_by_state(src_st, state.in=state.in, MAorder=MAorder, Difforder=Difforder, basedate=basedate,
+         lookback_days=lookback_days, ARorder=ARorder, max_date=max_date)
    cbyd_shim = dplyr::filter(src_st,  # shim
             date >= basedate & subset=="confirmed" & state==state.in)
-   ibyd_shim = form_inc_state(cbyd_shim, regtag=state.in)
+   ibyd_shim = form_inc_state(cbyd_shim, regtag=state.in, max_date=max_date)
    ibyd_shim$count = as.numeric(nat$tsfull)-as.numeric(st$tsfull)
-   .Arima_inc(ibyd_shim, state.in=state.in, MAorder=MAorder,
-      trend=trend, basedate=basedate, lookback_days=lookback_days, ARorder=ARorder)
+   .Arima_inc(ibyd_shim, state.in=paste("excl", state.in), MAorder=MAorder,
+      Difforder=Difforder, basedate=basedate, lookback_days=lookback_days, ARorder=ARorder,
+           max_date=max_date)
+   }
+
+#' @export
+Arima_drop_states = function(src_us, src_st, states.in= c("New York", "New Jersey"), MAorder=3, 
+   Difforder=1, basedate="2020-02-15", lookback_days=29, ARorder=0, max_date=NULL, ARorder.nat=3) {
+   nat = Arima_nation(src_us, MAorder=MAorder, Difforder=Difforder, basedate=basedate,
+         lookback_days=lookback_days, ARorder=ARorder.nat, max_date=max_date)
+   sts = lapply(states.in, function(x) Arima_by_state(src_st, state.in=x, 
+         MAorder=MAorder, Difforder=Difforder, basedate=basedate,
+         lookback_days=lookback_days, ARorder=ARorder, max_date=max_date))
+   names(sts) = states.in
+   cbyd_shims = lapply(states.in, function(x) dplyr::filter(src_st,  # shim
+            date >= basedate & subset=="confirmed" & state==x))
+   names(cbyd_shims) = states.in
+   ibyd_shims = lapply(states.in, function(x) form_inc_state(cbyd_shims[[x]], regtag=x, max_date=max_date))
+   ibyd_shim = ibyd_shims[[1]]
+   ibyd_shim$count = as.numeric(nat$tsfull)-as.numeric(sts[[1]]$tsfull)
+   for (i in 2:length(ibyd_shims)) 
+       ibyd_shim$count = as.numeric(ibyd_shim$count)-as.numeric(sts[[i]]$tsfull)
+   .Arima_inc(ibyd_shim, state.in=paste("excl", paste(states.in, collapse=", ")), MAorder=MAorder,
+      Difforder=Difforder, basedate=basedate, lookback_days=lookback_days, ARorder=ARorder,
+           max_date=max_date)
    }
 
 #' full incidence for contiguous states
 #' @inheritParams Arima_drop_state
+#' @param src tibble for state level data like that of nytimes_state_data()
 #' @param contig_vec character() vector of tokens for subsetting src
 #' @examples
 #' usd = usa_facts_data()
@@ -227,11 +348,11 @@ Arima_drop_state = function(src_us, src_st, state.in="New York", MAorder=2,
 #' plot(cont)
 #' @export
 Arima_contig_states = function(src, state.in="All contig", MAorder=2, 
-   trend="linear", basedate="2020-03-15", lookback_days=29, ARorder=0,
-   contig_vec = contig_states_twolet()) {
+   Difforder=1, basedate="2020-02-15", lookback_days=29, ARorder=0,
+   contig_vec = contig_states_twolet(), max_date=NULL) {
    cbyd = dplyr::filter(src, date >= basedate & 
        subset=="confirmed" & state %in% contig_vec)
-   ibyd = form_inc_state(cbyd, regtag=state.in)
+   ibyd = form_inc_state(cbyd, regtag=state.in, max_date=max_date)
    .Arima_inc(ibyd, state.in="all", MAorder=MAorder,
-      trend=trend, basedate=basedate, lookback_days=lookback_days, ARorder=ARorder)
+      Difforder=Difforder, basedate=basedate, lookback_days=lookback_days, ARorder=ARorder, max_date=max_date)
    }
--- inst/tsapp/ui.R
+++ inst/tsapp/ui.R
@@ -1,12 +1,9 @@
-attach_if_needed = function(pk) {
-  sr = search()
-  if (!(paste0("package:", pk) %in% sr)) library(pk, character.only=TRUE)
-}
 
-attach_if_needed("shiny")
-attach_if_needed("sars2pack")
-attach_if_needed("dplyr")
-attach_if_needed("forecast")
+library(shiny)
+library(sars2app)
+library(dplyr)
+library(forecast)
+library(shinytoastr)
 
 # private
 #make_cumul_events = function(count, dates, alpha3="USA", source="NYT", regtag=NA) {
@@ -27,12 +24,13 @@ attach_if_needed("forecast")
 #}
 #
 # ui code starts here
- basedate = "2020-03-15" # data prior to this date are dropped completely
+ basedate = "2020-02-15" # data prior to this date are dropped completely
  lookback_days = 29 # from present date
  if (!exists(".nyd.global")).nyd.global <<- nytimes_state_data() # cumulative
  if (!exists(".jhu.global")) .jhu.global <<- enriched_jhu_data() # cumulative
  allst = sort(unique(.nyd.global$state))
  ui = fluidPage(
+  shinytoastr::useToastr(),
   sidebarLayout(
    sidebarPanel(
     helpText("COVID-19 incidence trajectories with simple time series models.
@@ -40,35 +38,52 @@ Code derived from the ", a(href="https://seandavi.github.io/sars2pack", "sars2pa
 " data science and documentation portal.
 See 'About' tab for additional details."),
     selectInput("source", "source", choices=c("fullusa", allst), selected="fullusa"),
-    radioButtons("excl", "exclude from fullusa", choices=c("no", "New York", "Washington"), selected="no"),
-    selectInput("trigcomp", "# trig components", choices=c("3", "4", "5", "6"), selected="5"),
-    numericInput("MAorder", "MA order", min=1, max=4, value=2), # choices=c("1", "2"), selected="2"),
+    radioButtons("excl", "exclude from fullusa", choices=c("none", "New York", "NY,NJ", "NY,NJ,MA",
+                  "NY,NJ,MA,PA"), selected="none"),
+    dateInput("maxdate", "look back from", min="2020-03-15", max=max(lubridate::as_date(.nyd.global$date)),
+         value=max(lubridate::as_date(.nyd.global$date))),
+    numericInput("MAorder", "MA order", min=0, max=6, value=3), 
+    numericInput("ARorder", "AR order", min=0, max=6, value=3), # May 22 opt for USA
     numericInput("Difforder", "Difforder", min=0, max=2, value=1),
     actionButton("stopper", "stop app"),
     width=3),
    mainPanel(
     tabsetPanel(
      tabPanel("traj",
-      plotOutput("traj")
+      plotOutput("traj", height="425px"),
+      helpText("EpiEstim model for R[t] using MCMC-based Gamma model for serial interval:"),
+      plotOutput("Rtplot", height="425px")
       ),
      tabPanel("rept",
-      verbatimTextOutput("rept")
+      verbatimTextOutput("rept"),
+      plotOutput("tsdiag")
+      ),
+     tabPanel("meta",
+      verbatimTextOutput("meta.rept"),
+      plotOutput("metaplot", height=700)
       ),
      tabPanel("about",
       helpText("This app was produced to help evaluate a claim that
 an apparent decline in COVID-19 incidence for USA as a whole is driven by
 the actual decline in incidence in New York.  See ",
-a(href="https://www.erinbromage.com/post/the-risks-know-them-avoid-them",
-"Erin Bromage's blog post"),"  As a by-product, models can
+ a(href="https://www.erinbromage.com/post/the-risks-know-them-avoid-them",
+"Erin Bromage's blog post."),"  As a by-product, models can
 be fit for any US state.  The data are generated using `nytimes_state_data()`
 in sars2pack."),
-      helpText("Tab 'traj' is a plot of the last 29 days of incidence reports
-with a trace of the time series model as selected using the input controls."),
-      helpText("Tab 'rept' reports statistics from the `arima` function for
-the selected model."),
-      helpText("The trigonometric component of the model is inspired by ", 
-a(href="http://webpopix.org/covidix19.html#parameter-estimation-and-model-selection", "work of"),
-" Marc Lavielle.  The modeling used in this app is however completely elementary and uses only base R.")
+      helpText("Tab 'traj' include two displays.  At the top is a plot of the last 29 days of incidence reports
+with a trace of the fitted time series model as selected using the input controls.  Below that
+is a display of the fit of an `EpiEstim estimate_R` run with an MCMC-based
+model for the serial interval; see the ", a(href='https://github.com/seandavi/sars2pack',
+"doMCMC vignette of sars2pack.", target="_blank")),
+      helpText("Tab 'rept' reports statistics from the `forecast::Arima` function for
+the selected model, and display the result of tsdiag()."),
+      helpText("Tab 'meta' reports meta-analysis results using the rmeta package.
+The time series model for each state employs autoregressive and moving average
+orders selected by minimizing BIC over a grid of choices ranging from 0 to 5
+for each parameter.  Note that this minimization was conducted for data available
+20 May 2020.  Users can update these selections in their own
+interactive R sessions using 'min_bic_all_states' and supplying the result to `run_meta`."),
+      helpText(paste("This app is defined in", packageVersion("sars2pack"), "of sars2pack."))
       )
      )
     )
