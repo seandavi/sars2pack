dd0431ef57cb3e9ab12f91c3b6da82d50d8529dc
Sean Davis <seandavi@gmail.com>
2020-12-20 10:17:21 -0500

    fixing missing globals
    - arima
    - data catalog

--- R/Arima.R
+++ R/Arima.R
@@ -175,7 +175,7 @@ form_inc_nation = function(src, regtag, max_date=NULL) {
 #' @export
 Arima_by_state = function(src, state.in="New York", MAorder=2, 
    Difforder=1, basedate="2020-02-15", lookback_days=29, ARorder=0, max_date=NULL) {
-   cbyd = dplyr::filter(src, date >= basedate & subset=="confirmed" & state==state.in) 
+   cbyd = dplyr::filter(src, date >= basedate & subset=="confirmed" & .data$state==state.in) 
    ibyd = form_inc_state(cbyd, regtag=state.in, max_date=max_date)
    tc = match.call()
    .Arima_inc(ibyd, state.in=state.in, MAorder=MAorder,
@@ -202,7 +202,7 @@ Arima_by_state = function(src, state.in="New York", MAorder=2,
 #' @export
 Arima_nation = function(ejhu, alp3="USA", MAorder=2,
    Difforder=1, basedate="2020-02-15", lookback_days=29, ARorder=0, max_date=NULL) {
-   cbyd = dplyr::filter(ejhu, date >= basedate & subset=="confirmed" & alpha3Code==alp3)
+   cbyd = dplyr::filter(ejhu, date >= basedate & subset=="confirmed" & .data$alpha3Code==alp3)
    ibyd = form_inc_nation(cbyd, regtag=alp3, max_date=max_date)
    tc = match.call()
    .Arima_inc(ibyd, state.in=alp3, MAorder=MAorder,
@@ -214,14 +214,14 @@ Arima_nation = function(ejhu, alp3="USA", MAorder=2,
    Difforder=1, basedate="2020-02-15", lookback_days=29, ARorder=0, max_date=NULL, topcall=NULL) {
    if (is.null(MAorder) | is.null(ARorder)) stop("MA/AR order inputs cannot be NULL")
    iuse = trim_from(ibyd, basedate)
-   full29 = tail(ibyd$count,lookback_days)
-   dates29 = tail(ibyd$date,lookback_days)
+   full29 = utils::tail(ibyd$count,lookback_days)
+   dates29 = utils::tail(ibyd$date,lookback_days)
    nlb=lookback_days-1
    time = (0:nlb)/nlb
 
    origin = max(ibyd$date)-lookback_days+1
    time.from.origin = as.numeric(dates29-origin)
-   tsfull = ts(full29, freq=1)
+   tsfull = stats::ts(full29, freq=1)
    Arima.full = try(Arima(tsfull, order=c(ARorder,Difforder,MAorder), include.drift=TRUE))
    if (inherits(Arima.full, "try-error")) {
      print(c(ARorder,Difforder,MAorder))
@@ -307,7 +307,7 @@ Arima_drop_state = function(src_us, src_st, state.in="New York", MAorder=2,
    st = Arima_by_state(src_st, state.in=state.in, MAorder=MAorder, Difforder=Difforder, basedate=basedate,
          lookback_days=lookback_days, ARorder=ARorder, max_date=max_date)
    cbyd_shim = dplyr::filter(src_st,  # shim
-            date >= basedate & subset=="confirmed" & state==state.in)
+            date >= basedate & subset=="confirmed" & .data$state==state.in)
    ibyd_shim = form_inc_state(cbyd_shim, regtag=state.in, max_date=max_date)
    ibyd_shim$count = as.numeric(nat$tsfull)-as.numeric(st$tsfull)
    .Arima_inc(ibyd_shim, state.in=paste("excl", state.in), MAorder=MAorder,
@@ -325,7 +325,7 @@ Arima_drop_states = function(src_us, src_st, states.in= c("New York", "New Jerse
          lookback_days=lookback_days, ARorder=ARorder, max_date=max_date))
    names(sts) = states.in
    cbyd_shims = lapply(states.in, function(x) dplyr::filter(src_st,  # shim
-            date >= basedate & subset=="confirmed" & state==x))
+            date >= basedate & subset=="confirmed" & .data$state==x))
    names(cbyd_shims) = states.in
    ibyd_shims = lapply(states.in, function(x) form_inc_state(cbyd_shims[[x]], regtag=x, max_date=max_date))
    ibyd_shim = ibyd_shims[[1]]
