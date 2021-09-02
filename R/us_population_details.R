d1a0ad2f294bfd90b80e738d6d274aa9427dde14
Sean Davis <seandavi@gmail.com>
2021-09-02 06:02:22 -0600

    actually tidycensus handles key in env variable

--- R/us_population_details.R
+++ R/us_population_details.R
@@ -2,8 +2,6 @@
 #'
 #' Get data from the US Census Bureau Population Estimates APIs.
 #'
-#' @param key a us census API key. See the help for \code{\link[tidycensus]{get_estimates}}
-#'   for setting up. 
 #' @param \dots Passed along to \code{\link[tidycensus]{get_estimates}}. Typical 
 #'   arguments would be something like `(geography='county', product='population', year=2019)`.
 #' 
@@ -19,7 +17,7 @@
 #' head(res)
 #'
 #' @export
-us_population_details <- function(key = Sys.getenv('CENSUS_API_KEY'), ...) {
+us_population_details <- function(...) {
   if(!requireNamespace("tidycensus")) {
     message("The tidycensus package is required for this ")
     message("functionality. Please install it first.")
